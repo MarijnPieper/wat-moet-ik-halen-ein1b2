7406c61a7831c8b264df4af9a9a34f9ae9f936a8
Laura Smit <laura__smit@live.nl>
2012-05-10 09:16:22 +0200

    Initiele commit

---
+++ src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
@@ -0,0 +1,13 @@
+package nl.saxion.ein1b2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatmoetikhalenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
