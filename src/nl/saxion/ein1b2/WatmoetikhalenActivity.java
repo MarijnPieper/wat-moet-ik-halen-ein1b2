9c44240c61aa70b0d28c5d4f14a4f4884f609228
Laura Smit <laura__smit@live.nl>
2012-05-10 09:36:54 +0200

    laura's test

--- src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
+++ src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
@@ -9,5 +9,6 @@ public class WatmoetikhalenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //hallo dit is laura's test
     }
 }
