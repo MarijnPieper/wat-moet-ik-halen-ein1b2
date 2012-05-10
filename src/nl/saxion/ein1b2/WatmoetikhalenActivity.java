bec32ab3b84d2be954953e9a43f2ecbe0568ed06
Tom Schokkenbroek <Tomschok@gmail.com>
2012-05-10 09:39:14 +0200

    Test van Tom

--- src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
+++ src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
@@ -9,5 +9,7 @@ public class WatmoetikhalenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Zebrapad
     }
 }
