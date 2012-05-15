cb9d3fc5a930fbb6cc0869ffcdfd52c96d0a5237
Marijn Pieper <marijno_pieper@hotmail.com>
2012-05-15 10:10:29 +0200

    Marijn

--- src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
+++ src/nl/saxion/ein1b2/WatmoetikhalenActivity.java
@@ -5,25 +5,11 @@ import android.os.Bundle;
 
 public class WatmoetikhalenActivity extends Activity {
     /** Called when the activity is first created. */
-	private DbAdapter dbHelper;
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.vakkenpakket_layout);
-        
-        
-        
-        
-    
-    	
-    
-=======
-        setContentView(R.layout.main);
-        
-        dbHelper = new DbAdapter(this);
-        dbHelper.open();
->>>>>>> 70c927c13df78cd60e14faa11d6f8d4ea0eb3ec0
+
     }
 }
