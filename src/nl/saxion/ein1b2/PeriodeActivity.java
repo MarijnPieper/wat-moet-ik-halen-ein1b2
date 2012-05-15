a50b75e81885671e3fb9780c0d400cfac8582284
Tom Schokkenbroek <Tomschok@gmail.com>
2012-05-15 09:03:00 +0200

    Test

--- src/nl/saxion/ein1b2/PeriodeActivity.java
+++ src/nl/saxion/ein1b2/PeriodeActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class PeriodeActivity extends Activity {
@@ -21,16 +22,27 @@ public class PeriodeActivity extends Activity {
         setContentView(R.layout.periodes);
         
         ListView l = (ListView) findViewById(R.id.listViewPeriodes);
+        Button Plus = (Button) findViewById(R.id.PeriodeAddVak);
+        Plus.setOnClickListener(new ClickListener());
+        
         periodes = new ArrayList<Periode>();
         periodes.add(new Periode("Kwartiel 1", new GregorianCalendar(2012,1,1), new GregorianCalendar(2012,4,4)));
         periodes.add(new Periode("Kwartiel 2", new GregorianCalendar(2012,1,1), new GregorianCalendar(2012,4,4)));
         adapter = new PeriodesAdapter(this, R.layout.periodeadapter, periodes);
        
         l.setAdapter(adapter);
-        l.setOnItemClickListener(new ClickListener());
+        l.setOnItemClickListener(new ItemClickListener());
     }
     
-    class ClickListener implements OnItemClickListener {
+    class ClickListener implements OnClickListener {
+
+		public void onClick(View v) {
+				
+		}
+    	
+    }
+    
+    class ItemClickListener implements OnItemClickListener {
 
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
