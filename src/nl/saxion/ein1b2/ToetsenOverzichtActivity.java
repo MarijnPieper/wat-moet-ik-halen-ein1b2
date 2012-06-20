a1b061c67a9d26248df9a7a95e95ebc267d24c94
Tom Schokkenbroek <Tomschok@gmail.com>
2012-06-20 09:09:54 +0200

    Test

--- src/nl/saxion/ein1b2/ToetsenOverzichtActivity.java
+++ src/nl/saxion/ein1b2/ToetsenOverzichtActivity.java
@@ -30,8 +30,8 @@ public class ToetsenOverzichtActivity extends Activity implements OnCheckedChang
 	int vakid;
 	
 	
-	 @Override
-	 public void onCreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 requestWindowFeature(Window.FEATURE_NO_TITLE);
 		 getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
